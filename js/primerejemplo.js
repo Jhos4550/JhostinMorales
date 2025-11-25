03af301d7b0df31c4f22049b5db29fb70e01c4d6
Jhos4550 <jhostin4550@gmail.com>
2025-11-25 14:13:28 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -2,11 +2,11 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').src='images/sad.png';
+    document.getElementById('carita').src='imagenes/sad.png';
     alert("Po que ta triste mano");
 }
 function feliz(){
-    document.getElementById('carita').src='images/happy.png';
+    document.getElementById('carita').src='imagenes/happy.png';
 }
 function cambiarColor(){
     document.getElementById('menu').style.color='#eed33aff';
