a58bd738a28eba75cddb65301c4d54a03f29f1ce
Jhos4550 <jhostin4550@gmail.com>
2025-11-25 13:59:27 -0500

    k

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -1,6 +1,6 @@
 function triste(){
     document.getElementById('carita').src='imagenes/trite.png';
-    alert; 
+    alert; poque tite ta tite la wawa
 }
 function feliz(){
     document.getElementById('carita').src='imagenes/feliz.png';
