1dd21aaffe7e25c4abf2cd79632eab2cfc721d4a
Jhos4550 <jhostin4550@gmail.com>
2025-11-25 13:22:17 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -1,18 +1,18 @@
 function triste(){
-    document.getElementById('carita').src='images/trite.png';
+    document.getElementById('carita').src='images/sad.png';
     alert;
 }
 function feliz(){
-    document.getElementById('carita').src='images/feliz.png';
+    document.getElementById('carita').src='images/happy.png';
 }
 function cambiarColor(){
-    document.getElementById('menu').style.background='#8000b3ff';
+    document.getElementById('menu').style.color='#e100ffff';
 }
 function cambiarLogo(){
     document.getElementById('logo').src='imagenes/logo2.png';
 }
 function cambiarFondo(){
-     document.getElementById('menu').style.background='#00ffeab7';
+     document.getElementById('menu').style.background='#00ff9dff';
 }
 function cambiodeTexto(){
     document.getElementById('Subtitulo').textContent="hjdf";
