f6aa8d48c998ae8392df96b4f1b67d33b74df129
Jhos4550 <jhostin4550@gmail.com>
2025-11-25 14:03:08 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -1,18 +1,18 @@
 function triste(){
-    document.getElementById('carita').src='imagenes/trite.png';
+    document.getElementById('carita').src='images/sad.png';
     alert; poque tite ta tite la wawa
 }
 function feliz(){
-    document.getElementById('carita').src='imagenes/feliz.png';
+    document.getElementById('carita').src='images/happy.png';
 }
 function cambiarColor(){
-    document.getElementById('menu').style.color='#e100ffff';
+    document.getElementById('menu').style.color='#c70909ff';
 }
 function cambiarLogo(){
     document.getElementById('logo').src='imagenes/logo2.png';
 }
 function cambiarFondo(){
-     document.getElementById('menu').style.background='#00ff9dff';
+     document.getElementById('menu').style.background='#b10099ff';
 }
 function cambiodeTexto(){
     document.getElementById('Subtitulo').textContent="hjdf";
