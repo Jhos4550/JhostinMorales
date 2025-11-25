49762cb3d05c60960bff074460cf60f9acfabc67
Jhos4550 <jhostin4550@gmail.com>
2025-11-25 12:43:38 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -10,7 +10,7 @@ function cambiarColor(){
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='images/logo2.jpg';
+    document.getElementById('logo').src='imagenes/logo2';
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#524604';
