24cf61ee661f49d249b9ded0983e58b8278b73d4
Jhos4550 <jhostin4550@gmail.com>
2025-11-26 13:17:29 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -2,24 +2,24 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').src='images/sad.png';
+    document.getElementById('carita').src='imagenes/trite.png';
     alert("Po que ta triste mano");
 }
 function feliz(){
-    document.getElementById('carita').src='images/happy.png';
+    document.getElementById('carita').src='imagenes/feliz.png';
 }
 function cambiarColor(){
     document.getElementById('menu').style.color='#eed33aff';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='images/logo2.jpg';
+    document.getElementById('logo').src='imagenes/logo2.jpg';
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#524604';
 }
 function cambiodeTexto(){
-    document.getElementById('Subtitulo').textContent="YA LE SE AL JS";
+    document.getElementById('Subtitulo').textContent="dNo cambies p";
 }
 function fondoAside(){
     document.getElementById('aside').style.background='#f584f5ff';
