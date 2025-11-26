f2c16297a00031234570e7ff8163bef7c5590aeb
Jhos4550 <jhostin4550@gmail.com>
2025-11-26 13:28:04 -0500

    w

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -22,5 +22,5 @@ function cambiodeTexto(){
     document.getElementById('Subtitulo').textContent="No cambies p";
 }
 function fondoAside(){
-    document.getElementById('aside').style.background='#f584f5ff';
+    document.getElementById('aside').style.background='#00ffb3ff';
 }
