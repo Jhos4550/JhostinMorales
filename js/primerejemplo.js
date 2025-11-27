97657a394a2ac50a4a38a3657e92554000eb2240
Jhos4550 <jhostin4550@gmail.com>
2025-11-27 11:54:51 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -2,25 +2,25 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').src='imagenes/trite.png';
-    alert("Po que ta trite la wawa");
+    document.getElementById('carita').src='images/sad.png';
+    alert("Po que ta triste mano");
 }
 function feliz(){
-    document.getElementById('carita').src='imagenes/feliz.png';
+    document.getElementById('carita').src='images/happy.png';
 }
 function cambiarColor(){
-    document.getElementById('menu').style.color='#00ff88ff';
+    document.getElementById('menu').style.color='#eed33aff';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='imagenes/logo2.png';
+    document.getElementById('logo').src='images/logo2.jpg';
 }
 function cambiarFondo(){
-     document.getElementById('menu').style.background='#8f7900ff';
+     document.getElementById('menu').style.background='#524604';
 }
 function cambiodeTexto(){
-    document.getElementById('Subtitulo').textContent="No cambies p";
+    document.getElementById('Subtitulo').textContent="YA LE SE AL JS";
 }
 function fondoAside(){
-    document.getElementById('aside').style.background='#4d004dff';
+    document.getElementById('aside').style.background='#f584f5ff';
 }
