a4745a3bdf03141c377b8b412eafb8a9a95343e8
Jhos4550 <jhostin4550@gmail.com>
2025-11-27 19:16:12 -0500

    d

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -4,23 +4,23 @@ alert("Hola Mundo, esto es un alert")
 function avion(){
     document.getElementById('avion').src='imagenes/gemela.jpg';
 }
+
 function gemela(){
     document.getElementById('gemela').src='imagenes/gemela.jpg'
 }
 
-
-
-
-
 function cambiarLogo(){
     document.getElementById('logo').src='imagenes/logo2.png';
 }
+
 function cambiarFondo(){
     document.getElementById('menu').style.background='#00e1ffff';
 }
+
 function cambiodeTexto(){
     document.getElementById('Subtitulo').textContent="No cambies p";
 }
+
 function fondoAside(){
     document.getElementById('aside').style.background='#2fff5cff';
 }
