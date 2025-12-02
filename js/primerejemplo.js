c6f17de15c343e9e177ff0dbeb9386bfc5f8ad24
Jhos4550 <jhostin4550@gmail.com>
2025-12-02 13:58:21 -0500

    d

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -2,11 +2,11 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert.") 
 
 function avion(){
-    document.getElementById('avion').src='imagenes/avion.jpg';
+    document.getElementById('carita').src='imagenes/avion.jpg';
 }
 
 function torres(){
-    document.getElementById('torres').src='imagenes/torres.jpg';
+    document.getElementById('carita').src='imagenes/torres.jpg';
 }
 
 function cambiarLogo(){
