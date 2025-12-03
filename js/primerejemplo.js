6945a9e38c9340bf62ddec0b043dc3ee4d33cd58
Jhos4550 <jhostin4550@gmail.com>
2025-12-03 13:18:13 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -6,7 +6,7 @@ function torres(){
     alert("Este avion a destruido a las torres gemelas") 
 } 
 function destruido(){
-    document.getElementById('destruido').src='imagenes/destruido.jpg';
+document.getElementById("miBoton").src='imagenes/destruido.png';
     alert("F bb") 
 } 
 function cambiarLogo(){
