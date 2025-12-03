aa5fb958eaf93c1805ccbce2e927755ade8f1e44
Jhos4550 <jhostin4550@gmail.com>
2025-12-03 11:28:14 -0500

    k

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -4,19 +4,22 @@ alert("Hola Mundo, esto es un alert")
 function avion(){
     document.getElementById('avion').src='imagenes/avion.jpg';
 }
-function torres(){
-    document.getElementById('torres').src='imagenes/torres.jpg';
+function feliz(){
+    document.getElementById('carita').src='imagenes/torres.jpg';
 }
 function cambiarColor(){
-    document.getElementById('menu').style.color='rgba(145, 0, 0, 0.76)';
+    document.getElementById('menu').style.color='#00ff62ff';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='imagenes/logo2.jpg';
+    document.getElementById('logo').src='imagenes/logo2.png';
+}
+function cambiarFondo(){
+     document.getElementById('menu').style.background='#524604';
 }
 function cambiodeTexto(){
-    document.getElementById('Subtitulo').textContent="No camvies p";
+    document.getElementById('Subtitulo').textContent="No cambies p";
 }
 function fondoAside(){
-    document.getElementById('aside').style.background='#7cff70ff';
+    document.getElementById('aside').style.background='#000658ff';
 }
