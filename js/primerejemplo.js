bfcaf4892d0553792d71a86ec94af907076ac446
Jhos4550 <jhostin4550@gmail.com>
2025-12-03 13:04:22 -0500

    s

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -3,12 +3,15 @@ alert("Hola Mundo, esto es un alert")
 
 function avion(){
     document.getElementById('avion').src='imagenes/torres.jpg';
+    alert("Este avion destryo a las torres gemelas") 
 }
 function torres(){
     document.getElementById('avion').src='imagenes/avion.jpg';
+    alert("Aqui estan las torres aun intactas") 
 } 
 function destruido(){
     document.getElementById('destruido').src='imagenes/destruido.jpg';
+    alert("Impacto el avion en las torres, pobres torres :,c") 
 } 
 function cambiarLogo(){
     document.getElementById('logo').src='imagenes/logo2.png';
