1083f88bbfb37a801a8de7fe5c0495b0f078e32f
Jhos4550 <jhostin4550@gmail.com>
2025-12-03 13:30:33 -0500

    yt

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -1,23 +1,28 @@
 console.log("Hola Mundo");
-alert("Hola Mundo, esto es un alert") 
- 
+alert("Hola Mundo, esto es un alert");
+
 function torres(){
-    document.getElementById('avion').src='imagenes/avion.jpg';
-    alert("Este avion a destruido a las torres gemelas") 
-} 
+    document.getElementById('avion').src = 'imagenes/avion.jpg';
+    alert("Este avion ha destruido a las torres gemelas");
+}
+
 function destruido(){
-    document.getElementById("destrido").src = 'imagenes/destruido.png';
-    alert("F bb") 
-} 
+    document.getElementById('avion').src = 'imagenes/destruido.png';
+    alert("F bb");
+}
+
 function cambiarLogo(){
-    document.getElementById('logo').src='imagenes/logo2.png';
+    document.getElementById('logo').src = 'imagenes/logo2.png';
 }
+
 function cambiarFondo(){
-     document.getElementById('menu').style.background='#8a0000ff';
+    document.getElementById('menu').style.background = '#8a0000ff';
 }
+
 function cambiodeTexto(){
-    document.getElementById('Subtitulo').textContent="No cambies p";
+    document.getElementById('Subtitulo').textContent = "No cambies p";
 }
+
 function fondoAside(){
-    document.getElementById('aside').style.background='#009919ff';
+    document.getElementById('aside').style.background = '#009919ff';
 }
